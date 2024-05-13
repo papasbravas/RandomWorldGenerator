bc78faa236d21e946c315a928e37134c9e1804df
papasbravas <aeg2809@gmail.com>
2024-05-13 13:22:08 +0200

    Ejercicio 23

--- Random/RandomWorldGenerator/src/randomworldgenerator/RandomWorldGenerator.java
+++ Random/RandomWorldGenerator/src/randomworldgenerator/RandomWorldGenerator.java
@@ -16,6 +16,7 @@ public class RandomWorldGenerator {
      */
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        System.out.println("Buenas tardes");
     }
     
 }
